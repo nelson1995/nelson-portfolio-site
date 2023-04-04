44cd2695f97dc004bdbd4e151fbc374b6f4d5a91
Nelson K <katalenelson@gmail.com>
2023-04-04 15:53:58 +0300

    renamed base to publicPath in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  base: '/nelson-portfolio-site/'
+  publicPath: '/nelson-portfolio-site/'
 })

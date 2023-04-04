5b357fca8ee14514ac0cac1cbb1106fde1e893c6
Nelson K <katalenelson@gmail.com>
2023-04-04 15:40:43 +0300

    modified gh-pages-deploy.js

--- Scripts/gh-pages-deploy.js
+++ Scripts/gh-pages-deploy.js
@@ -11,11 +11,11 @@ const fs = require("fs");
     const folderName = fs.existsSync("dist") ? "dist" : "build";
     await execa("git", ["--work-tree", folderName, "add", "--all"]);
     await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
-    console.log("Pushing to gh-pages...");
+    console.log("Pushing to branch gh-pages...");
     await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
     await execa("rm", ["-r", folderName]);
     await execa("git", ["checkout", "-f", "master"]);
-    console.log("Vue app successfully deployed. Check your repository settings");
+    console.log("Vue app successfully deployed. Check your url in repository settings > pages.");
   } catch (e) {
     // eslint-disable-next-line no-console
     console.log(e.message);
